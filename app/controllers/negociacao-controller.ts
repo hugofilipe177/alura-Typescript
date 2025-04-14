d61ea995b38cb04ccdde851f88a4586023acf062
Hugo Filipe <kylon9867@gmail.com>
2025-04-14 15:13:49 -0300

    Final

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,18 +1,22 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+
 
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement; 
     private inputValor: HTMLInputElement; 
+    private negociacoes = new Negociacoes();
     constructor(){
         this.inputData = document.querySelector("#data");
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
     adiciona(): void{
-        const negociacao = this.criaNegociacao
-        console.log(negociacao);
+        const negociacao = this.criaNegociacao();
+        this.negociacoes.adicionar(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
         
     }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -2,4 +2,11 @@ export class Negociacoes {
     constructor() {
         this.negociacoes = [];
     }
+    adicionar(negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+    lista() {
+        return [...this.negociacoes];
+    }
 }
+const negociacoes = new Negociacoes();
